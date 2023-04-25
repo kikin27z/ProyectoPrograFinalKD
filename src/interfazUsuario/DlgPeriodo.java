26f1ab5b2722767d614ca3c0fec180b87e5be8ca
karim <karim@LAPTOP-RII70H89>
2023-04-25 09:05:47 -0700

    Terminé el DlgPeriodo.

--- src/interfazUsuario/DlgPeriodo.java
+++ src/interfazUsuario/DlgPeriodo.java
@@ -1,21 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
- */
 package interfazUsuario;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import objetosServicio.Fecha;
+import objetosServicio.Periodo;
 
 /**
  *
- * @author lv1013
+ * @author Diego Valenzuela Parra y José Karim Franco Valencia
  */
 public class DlgPeriodo extends javax.swing.JDialog {
 
     /**
      * Creates new form DlgPeriodo
      */
-    public DlgPeriodo(java.awt.Frame parent, boolean modal) {
-        super(parent, modal);
+    public DlgPeriodo(java.awt.Frame parent, String title, boolean modal, Periodo periodo, int operacion, StringBuffer respuesta) {
+        super(parent, title, modal);
+        this.periodo = periodo;
+        this.operacion = operacion;
+        this.respuesta = respuesta;
         initComponents();
+
+        // Establece el valor por omisión para respuesta, por si se cierra el
+        // cuadro de diálogo presionando el botón cerrar o el botón cancelar
+        respuesta.append(ConstantesGUI.CANCELAR);
+        // centra el cuadro de dialogo sobre la ventana de la aplicación
+        centraCuadroDialogo(parent);
+
+        // Muestra el cuadro de diálogo
+        setVisible(true);
+    }
+
+    /**
+     * Este método centra el cuadro de dialogo sobre la ventana de la
+     * aplicación.
+     *
+     * @param parent Ventana sobre la que aparecerá el cuadro de diálogo
+     */
+    private void centraCuadroDialogo(java.awt.Frame parent) {
+        // Obtiene el tamaño y posición de la ventana de la aplicación
+        Dimension frameSize = parent.getSize();
+        Point loc = parent.getLocation();
+        // Obtiene el tamaño del cuadro de diálogo
+        Dimension dlgSize = getPreferredSize();
+
+        // Centra el cuadro de diálogo sobre la ventana padre
+        setLocation((frameSize.width - dlgSize.width) / 2 + loc.x,
+                (frameSize.height - dlgSize.height) / 2 + loc.y);
     }
 
     /**
@@ -27,22 +58,134 @@ public class DlgPeriodo extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        campoTextoDesde = new javax.swing.JTextField();
+        campoTextoHasta = new javax.swing.JTextField();
+        botonAceptar = new javax.swing.JButton();
+        botonRestaurar = new javax.swing.JButton();
+        botonCancelar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel1.setText("Desde:");
+
+        jLabel2.setText("Hasta:");
+
+        botonAceptar.setText("Aceptar");
+        botonAceptar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonAceptarActionPerformed(evt);
+            }
+        });
+
+        botonRestaurar.setText("Restaurar");
+        botonRestaurar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonRestaurarActionPerformed(evt);
+            }
+        });
+
+        botonCancelar.setText("Cancelar");
+        botonCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonCancelarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(botonAceptar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(botonRestaurar)
+                        .addGap(55, 55, 55)
+                        .addComponent(botonCancelar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(campoTextoDesde, javax.swing.GroupLayout.PREFERRED_SIZE, 286, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(campoTextoHasta, javax.swing.GroupLayout.PREFERRED_SIZE, 286, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(97, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(campoTextoDesde, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(campoTextoHasta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(59, 59, 59)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(botonAceptar)
+                    .addComponent(botonRestaurar)
+                    .addComponent(botonCancelar))
+                .addGap(23, 23, 23))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    /**
+     * Método oyente del botón botonAceptar
+     *
+     */
+    private void botonAceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonAceptarActionPerformed
+        // Toma los valores capturados en los campos de texto y en la caja
+        // combinada y almacénalos en el parámetro cancion.
+        periodo.setDesde(new Fecha(campoTextoDesde.getText()));
+        periodo.setHasta(new Fecha(campoTextoDesde.getText()));
+
+        // Borra el contenido de respuesta
+        respuesta.delete(0, respuesta.length());
+        // Establece que se presionó el botón botonAceptar
+        respuesta.append(ConstantesGUI.ACEPTAR);
+        // Destruye el cuadro de díalogo
+        dispose();
+    }//GEN-LAST:event_botonAceptarActionPerformed
+    
+    /**
+     * Método oyente del botón botonRestaurar
+     *
+     */
+    private void botonRestaurarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonRestaurarActionPerformed
+        // Restaura el contenido de los campos de texto a su valor original
+        // Si la operación es Agregar
+        campoTextoDesde.setText("");
+        campoTextoHasta.setText("");
+    }//GEN-LAST:event_botonRestaurarActionPerformed
+
+    /**
+     * Método oyente del botón botonAceptar
+     *
+     */
+    private void botonCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonCancelarActionPerformed
+        // Destruye el cuadro de díalogo
+        dispose();
+    }//GEN-LAST:event_botonCancelarActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botonAceptar;
+    private javax.swing.JButton botonCancelar;
+    private javax.swing.JButton botonRestaurar;
+    private javax.swing.JTextField campoTextoDesde;
+    private javax.swing.JTextField campoTextoHasta;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
+    private Periodo periodo;
+    private int operacion;
+    private StringBuffer respuesta;
 }
